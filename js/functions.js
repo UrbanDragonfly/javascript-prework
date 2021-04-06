15d56460ef97f12200a87cebaa060e5bb536cdc9
Michal Kozlowski <michal.a.kozlowski@live.com>
2021-04-06 15:58:38 +0200

    task 2 from 5.4 - player  move - done in functions.js

--- js/functions.js
+++ js/functions.js
@@ -8,16 +8,34 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+
+//Player Move
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(playerInput =='2'){
+	playerMove = 'papier';
+} else if(playerInput == '3'){
+	playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+//Computer Move
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
-
 if(randomNumber == 1){
   	computerMove = 'kamień';
 } else if(randomNumber == 2) {
-	computerMove = 'nożyce'; 
+	computerMove = 'papier'; 
 } else if(randomNumber == 3 ) {
-	computerMove = 'papier'
+	computerMove = 'nożyce'
 }
-
 printMessage('Mój ruch to: ' + computerMove);
 
