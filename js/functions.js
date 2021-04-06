b37515ec687ab2b1f9608e55b8bcdac20595491b
Michal Kozlowski <michal.a.kozlowski@live.com>
2021-04-06 16:56:15 +0200

    task 3 from 5.4 - game result - done in functions.js

--- js/functions.js
+++ js/functions.js
@@ -15,13 +15,15 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
+let playerMove = 'nieznany ruch';
+
 if(playerInput == '1'){
   playerMove = 'kamień';
 } else if(playerInput =='2'){
 	playerMove = 'papier';
 } else if(playerInput == '3'){
 	playerMove = 'nożyce';
-}
+} 
 
 printMessage('Twój ruch to: ' + playerMove);
 
@@ -39,3 +41,26 @@ if(randomNumber == 1){
 }
 printMessage('Mój ruch to: ' + computerMove);
 
+
+//Game Result
+if( playerMove == 'nieznany ruch'){
+	printMessage('Nie rozumiem co pokazałeś :-(')
+} else if( computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage('Ja wygrywam!');
+} else if( computerMove == 'kamień' && playerMove == 'kamień'){
+	printMessage('Remis!');
+} else if( computerMove == 'nożyce' && playerMove == 'papier'){
+	printMessage('Ja wygrywam!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+	printMessage('Remis!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Ty wygrywasz!');
+} else if( computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Ty wygrywasz!');
+} else if( computerMove == 'papier' && playerMove == 'kamień'){
+	printMessage('Ja wygrywam!');
+} 
