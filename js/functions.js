fb7b78381a41c31a5d346693dea5cb6c6bc28e2f
Michal Kozlowski <michal.a.kozlowski@live.com>
2021-04-07 22:04:28 +0200

    fix Listeners brackets, change let to const, apply {} to whole script

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+{
 function playGame(playerInput){
 	clearMessages();
 
@@ -33,7 +34,7 @@ function playGame(playerInput){
 	}
 
 	function printMessage(msg){
-		let div = document.createElement('div');
+		const div = document.createElement('div');
 		div.innerHTML = msg;
 		document.getElementById('messages').appendChild(div);
 	}
@@ -44,15 +45,14 @@ function playGame(playerInput){
 	}
 
 	//Player Move
-	//let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 	console.log('Gracz wpisał: ' + playerInput);
-	let playerMove = getMoveName(playerInput);
+	const playerMove = getMoveName(playerInput);
 	console.log ('ruch gracza to ' + playerMove);
 
 	//Computer Move
-	let randomNumber = Math.floor(Math.random() * 3 + 1);
+	const randomNumber = Math.floor(Math.random() * 3 + 1);
 	console.log('Wylosowana liczba to: ' + randomNumber);
-	let computerMove = getMoveName(randomNumber);
+	const computerMove = getMoveName(randomNumber);
 	console.log ('Ruch komputera to ' + computerMove);
 
 	//Game Result
@@ -64,12 +64,13 @@ function playGame(playerInput){
 document.getElementById("play-rock").addEventListener("click", function() {
 	console.log ('button clicked play-rock');
 	playGame(1);
-  });
+});
 document.getElementById("play-paper").addEventListener("click", function() {
 	console.log ('button clicked play-paper');
 	playGame(2);
-  });
+});
 document.getElementById("play-scissors").addEventListener("click", function() {
 	console.log ('button clicked play-scissors');
 	playGame(3);
-  });
+});
+}
